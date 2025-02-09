ecf473426ffeed907e134a5dfb2a77629b4d5453
davr66 <davisreis11@gmail.com>
2025-02-09 13:10:14 -0300

    feat: Adição da navbar e ajustes no footer.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer/Footer'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar></NavBar>
       <Footer></Footer>
     </>
   )
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import './NavBar.css'
+
+export default function NavBar(){
+    return(
+        <>
+            <nav>
+                <img id="logo" src="./assets/images/logo.png" alt="Logo da página"/>
+                <ul>
+                    <li><a href="#introducao">Inicio</a></li>
+                    <li><a href="#noticias">Noticias</a></li>
+                    <li><a href="#videos">Dica do dia</a></li>
+                    <li><a href="#especialistas">Especialistas</a></li>
+                    <li><a href="#contato">Contato</a></li>
+                </ul>
+                <div id="search">
+                    <input type="text"/>
+                    <button type="submit"><img src="assets/images/search.png" alt="Pesquisar"/></button>    
+                </div>
+            </nav>
+        </>
+    )
+}
